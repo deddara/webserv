b48e3edd76b53e5ad3fb98ca47d5a0ff2af6a222
awerebea (21-school) <awerebea.21@gmail.com>
2020-12-16 21:10:20 +0300

    Add methods to parse server and location blocks

--- src/ConfParser.cpp
+++ src/ConfParser.cpp
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 11:40:21 by awerebea          #+#    #+#             */
-/*   Updated: 2020/12/15 21:29:38 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/12/16 21:09:10 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 ConfParser::ConfParser(std::string const & fpath): _filePath(fpath)
 {
 	_success = false;
-	_servers = nullptr;
 	parser();
 }
 
@@ -67,7 +66,8 @@ std::string				pickWord(std::string const &data, size_t *pos,
 																	size_t len)
 {
 	std::string			word;
-	while (*pos < len - 1 && !isspace(data[*pos]))
+	while (*pos < len - 1 && !isspace(data[*pos]) && data[*pos] != ';' \
+			&& data[*pos] != '{' && data[*pos] != '}')
 		word.push_back(data[(*pos)++]);
 	return word;
 }
@@ -87,26 +87,350 @@ std::string const		ConfParser::readConfFile(int fd)
 		data.append(buf);
 	}
 	if (ret < 0)
-		errorExit(1);
+		errorExit(1, "");
 	return data;
 }
 
-void					ConfParser::errorExit(int code)
+void					ConfParser::errorExit(int code,
+													std::string const & word)
 {
-	std::string const	errors[3] = {
+	std::string			errors[14] = {
 		"Error: config file is unavailable",
 		"Error: read fails",
-		"Error: incorrect config file structure"
+		"Error: config file syntax error",
+		"Error: invalid number of arguments in \"" + word + \
+			"\" directive",
+		"Error: unknown directive \"" + word + "\"",
+		"Error: host not found in \"" + word + "\" of the \"listen\" directive",
+		"Error: invalid port in \"" + word + "\" of the \"listen\" directive",
+		"Error: server name \"" + word + "\" has suspicious symbols",
+		"Error: directive \"" + word + "\" is not terminated by \";\"",
+		"Error: unexpected \"{\"",
+		"Error: number \"" + word + "\" has suspicious symbols",
+		"Error: limit client body \"" + word + "\" overflows",
+		"Error: location prefix is missing",
+		"Error: unexpected \"}\"",
 	};
 	std::cout << errors[code] << std::endl;
 	exit(1);
 }
 
+void					serverInit(Servers *server)
+{
+	server->host = "";
+	server->port = 0;
+	// server->serverName; //INFO
+	server->errorPage = "";
+	server->limitClientBody = -1;
+}
+
+int						checkStringInt(std::string const &word)
+{
+	size_t				len = word.length();
+	for (size_t i = 0; i < len; ++i)
+	{
+		if (!std::isdigit(word[i]))
+			return 1;
+	}
+	return 0;
+}
+
+int						checkSuspiciousSymbols(std::string const &word)
+{
+	size_t				len = word.length();
+	for (size_t i = 0; i < len; ++i)
+	{
+		if (word[i] == '/')
+			return 1;
+	}
+	return 0;
+}
+
+int						checkBrackets(char c)
+{
+	if (c == '{')
+		return 1;
+	if (c == '}')
+		return 2;
+	return 0;
+}
+
+void					ConfParser::serverBlockProc(std::string const &data,
+													size_t *pos, size_t len)
+{
+	_serverFields.push_back("host");
+	_serverFields.push_back("listen");
+	_serverFields.push_back("server_name");
+	_serverFields.push_back("error_page");
+	_serverFields.push_back("limit_client_body");
+	_serverFields.push_back("location");
+
+	skipSpaceComm(data, pos, len);
+	if (checkBrackets(data[*pos]) != 1)
+		errorExit(2, "");
+	(*pos)++;
+	Servers				server;
+	serverInit(&server);
+
+	std::string			key;
+	std::string			val;
+	int					ret;
+
+	while (checkBrackets(data[*pos]) != 2)
+	{
+		ret = 0;
+		skipSpaceComm(data, pos, len);
+		if (checkBrackets(data[*pos]) == 1)
+			errorExit(9, "");
+		key = pickWord(data, pos, len);
+		int				i = 0;
+		for (; i < 6; ++i)
+		{
+			if (toLower(key) == _serverFields[i])
+			{
+				skipSpaceComm(data, pos, len);
+				if (data[*pos] == ';')
+					errorExit(3, key);
+				break ;
+			}
+		}
+		if (i == 6)
+			errorExit(4, key);
+		skipSpaceComm(data, pos, len);
+		if (checkBrackets(data[*pos]) == 1)
+			errorExit(9, "");
+		val = pickWord(data, pos, len);
+		if (i == 0)
+		{
+			server.host = val;
+			skipSpaceComm(data, pos, len);
+			if ((ret = checkBrackets(data[*pos])))
+				errorExit(ret == 1 ? 9 : 8, key);
+			if (data[*pos] != ';')
+				errorExit(3, key);
+			(*pos)++;
+		}
+		else if (i == 1)
+		{
+			if (checkStringInt(val))
+				errorExit(5, val);
+			server.port = static_cast<int>(std::stold(val));
+			if (server.port < 0 || server.port > 65535)
+				errorExit(6, val);
+			skipSpaceComm(data, pos, len);
+			if ((ret = checkBrackets(data[*pos])))
+				errorExit(ret == 1 ? 9 : 8, key);
+			if (data[*pos] != ';')
+				errorExit(3, key);
+			(*pos)++;
+		}
+		else if (i == 2)
+		{
+			if (server.serverName.size())
+				server.serverName.clear();
+			if (checkBrackets(data[*pos]) == 1)
+				errorExit(9, "");
+			if (checkSuspiciousSymbols(val))
+				errorExit(7, val);
+			server.serverName.push_back(val);
+			skipSpaceComm(data, pos, len);
+			while (data[*pos] != ';')
+			{
+				if ((ret = checkBrackets(data[*pos])))
+					errorExit(ret == 1 ? 9 : 8, key);
+				val = pickWord(data, pos, len);
+				if (checkSuspiciousSymbols(val))
+					errorExit(7, val);
+				server.serverName.push_back(val);
+				skipSpaceComm(data, pos, len);
+			}
+			(*pos)++;
+		}
+		if (i == 3)
+		{
+			server.errorPage = val;
+			skipSpaceComm(data, pos, len);
+			if ((ret = checkBrackets(data[*pos])))
+				errorExit(ret == 1 ? 9 : 8, key);
+			if (data[*pos] != ';')
+				errorExit(3, key);
+			(*pos)++;
+		}
+		else if (i == 4)
+		{
+			if (checkStringInt(val))
+				errorExit(10, val);
+			server.limitClientBody = static_cast<int>(std::stold(val));
+			if (server.limitClientBody < 0)
+				errorExit(11, val);
+			skipSpaceComm(data, pos, len);
+			if ((ret = checkBrackets(data[*pos])))
+				errorExit(ret == 1 ? 9 : 8, key);
+			if (data[*pos] != ';')
+				errorExit(3, key);
+			(*pos)++;
+		}
+		if (i == 5)
+		{
+			locationBlockProc(data, pos, len, val);
+		}
+		std::cout << server.host << std::endl; //DEBUG
+		std::cout << server.port << std::endl; //DEBUG
+		for (unsigned long i = 0; i < server.serverName.size(); ++i)
+			std::cout << server.serverName[i] << std::endl; //DEBUG
+		std::cout << server.errorPage << std::endl; //DEBUG
+		std::cout << server.limitClientBody << std::endl; //DEBUG
+	}
+}
+
+void					locationInit(Locations *location)
+{
+	location->prefix = "";
+	// location->index; //INFO
+	// location->allowMethods; //INFO
+	location->root = "";
+	location->autoindex = "off";
+}
+
+void					ConfParser::locationBlockProc(std::string const &data,
+						size_t *pos, size_t len, std::string const &prefix)
+{
+	_locationsFields.push_back("index");
+	_locationsFields.push_back("allow_methods");
+	_locationsFields.push_back("root");
+	_locationsFields.push_back("autoindex");
+
+	int					ret;
+
+	if ((ret = checkBrackets(data[*pos])))
+		errorExit(ret == 1 ? 12 : 13, "");
+	skipSpaceComm(data, pos, len);
+
+	Locations			location;
+	locationInit(&location);
+	location.prefix = prefix;
+
+	if (location.prefix[0] != '/')
+		location.prefix.insert(0, "/", 1);
+
+	skipSpaceComm(data, pos, len);
+	if (checkBrackets(data[*pos]) != 1)
+		errorExit(2, "");
+	(*pos)++;
+
+	std::string			key;
+	std::string			val;
+
+	while (checkBrackets(data[*pos]) != 2)
+	{
+		ret = 0;
+		skipSpaceComm(data, pos, len);
+		if (checkBrackets(data[*pos]) == 1)
+			errorExit(9, "");
+		key = pickWord(data, pos, len);
+		int				i = 0;
+		for (; i < 4; ++i)
+		{
+			if (toLower(key) == _locationsFields[i])
+			{
+				skipSpaceComm(data, pos, len);
+				if (data[*pos] == ';')
+					errorExit(3, key);
+				break ;
+			}
+		}
+		if (i == 4)
+			errorExit(4, key);
+		skipSpaceComm(data, pos, len);
+		if (checkBrackets(data[*pos]) == 1)
+			errorExit(9, "");
+		val = pickWord(data, pos, len);
+		if (i == 0)
+		{
+			if (location.index.size())
+				location.index.clear();
+			if (checkBrackets(data[*pos]) == 1)
+				errorExit(9, "");
+			if (checkSuspiciousSymbols(val))
+				errorExit(7, val);
+			location.index.push_back(val);
+			skipSpaceComm(data, pos, len);
+			while (data[*pos] != ';')
+			{
+				if ((ret = checkBrackets(data[*pos])))
+					errorExit(ret == 1 ? 9 : 8, key);
+				val = pickWord(data, pos, len);
+				if (checkSuspiciousSymbols(val))
+					errorExit(7, val);
+				location.index.push_back(val);
+				skipSpaceComm(data, pos, len);
+			}
+			(*pos)++;
+		}
+		else if (i == 1)
+		{
+			if (location.allowMethods.size())
+				location.allowMethods.clear();
+			if (checkBrackets(data[*pos]) == 1)
+				errorExit(9, "");
+			if (checkSuspiciousSymbols(val))
+				errorExit(7, val);
+			location.allowMethods.push_back(val);
+			skipSpaceComm(data, pos, len);
+			while (data[*pos] != ';')
+			{
+				if ((ret = checkBrackets(data[*pos])))
+					errorExit(ret == 1 ? 9 : 8, key);
+				val = pickWord(data, pos, len);
+				if (checkSuspiciousSymbols(val))
+					errorExit(7, val);
+				location.allowMethods.push_back(val);
+				skipSpaceComm(data, pos, len);
+			}
+			(*pos)++;
+		}
+		else if (i == 2)
+		{
+			location.root = val;
+			skipSpaceComm(data, pos, len);
+			if ((ret = checkBrackets(data[*pos])))
+				errorExit(ret == 1 ? 9 : 8, key);
+			if (data[*pos] != ';')
+				errorExit(3, key);
+			(*pos)++;
+		}
+		else if (i == 3)
+		{
+			location.autoindex = val;
+			skipSpaceComm(data, pos, len);
+			if ((ret = checkBrackets(data[*pos])))
+				errorExit(ret == 1 ? 9 : 8, key);
+			if (data[*pos] != ';')
+				errorExit(3, key);
+			(*pos)++;
+		}
+		std::cout << location.prefix << std::endl; //DEBUG
+		for (unsigned long i = 0; i < location.index.size(); ++i)
+			std::cout << location.index[i] << std::endl; //DEBUG
+		for (unsigned long i = 0; i < location.allowMethods.size(); ++i)
+			std::cout << location.allowMethods[i] << std::endl; //DEBUG
+		std::cout << location.root << std::endl; //DEBUG
+		std::cout << location.autoindex << std::endl; //DEBUG
+	}
+}
+
 void					ConfParser::parser()
 {
+	std::vector<std::string>	confFields;
+	confFields.push_back("server");
+	confFields.push_back("location");
+	// bool				serverBlockInProgress = false;
+	_locationBlockInProgress = false;
+
+
 	int					fd;
 	if ((fd = open(_filePath.c_str(), O_RDONLY)) < 0)
-		errorExit(0);
+		errorExit(0, "");
 
 	std::string const	data = readConfFile(fd);
 
@@ -116,10 +440,10 @@ void					ConfParser::parser()
 	{
 		skipSpaceComm(data, &pos, len);
 		if (toLower(pickWord(data, &pos, len)) != "server")
-			errorExit(2);
+			errorExit(2, "");
 		else
 		{
-			std::cout << "!" << std::endl; //DEBUG
+			serverBlockProc(data, &pos, len);
 		}
 	}
 
--- src/ConfParser.hpp
+++ src/ConfParser.hpp
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 11:35:34 by awerebea          #+#    #+#             */
-/*   Updated: 2020/12/15 19:00:47 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/12/16 21:06:26 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,51 @@
 # define CONFPARSER_HPP
 
 # include <iostream>
+# include <list>
+# include <vector>
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft.h"
 
+struct Locations
+{
+	std::string					prefix;
+	std::vector<std::string>	index;
+	std::vector<std::string>	allowMethods;
+	std::string					root;
+	std::string					autoindex;
+};
+
+struct Servers
+{
+	std::string					host;
+	int							port;
+	std::vector<std::string>	serverName;
+	std::string					errorPage;
+	int							limitClientBody;
+	std::vector<Locations>		locations;
+};
+
 class							ConfParser
 {
-	struct Servers
-	{
-		std::string				host;
-		int						port;
-		std::string				server_name;
-
-		struct Servers *		next;
-	};
-
 	std::string					_filePath;
 	bool						_success;
-	Servers *					_servers;
+	std::vector<Servers>		_servers;
+	std::vector<std::string>	_serverFields;
+	std::vector<std::string>	_locationsFields;
+	bool						_locationBlockInProgress;
 
 								ConfParser();
 								ConfParser(ConfParser const &);
 	ConfParser &				operator=(ConfParser const &);
 
-	void						errorExit(int);
+	void						errorExit(int, std::string const &);
 	void						parser();
+	void						serverBlockProc(std::string const &,
+															size_t *, size_t);
+	void						locationBlockProc(std::string const &,
+										size_t *, size_t, std::string const &);
 	std::string const			readConfFile(int);
 public:
 								ConfParser(std::string const &);
